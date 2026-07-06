929db8535ceeac526e8ee13dec5dfd957d9280a2
Crezzyx8 <filberthuangg@gmail.com>
2026-07-06 16:24:21 +0700

    perubahan import

--- src/pages/MiniGames.jsx
+++ src/pages/MiniGames.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import TicTacToe from "../components/games/TicTacToe";
+import TicTacToe from "../components/Games/TicTacToe";
 
 function MiniGames() {
   return (
